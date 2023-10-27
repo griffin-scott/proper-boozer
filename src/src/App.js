72901da0203dee8cf5ffdfe3f1bd758494adc95b
Griffin Scott <griffinoscott@gmail.com>
2023-10-26 17:29:56 -0700

    initial commit

---
+++ src/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Pub from './components/Pub'
+
+const App = () => {
+  return (
+
+    <div className='App d-flex flex-column justify-content-center align-items-center h-100'>
+        <div className="col-6">
+            <h1 className='display-3 text-center text-light'>Proper Boozer</h1>
+            <p className='lead text-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <Pub />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/src/components/Pub.js
@@ -0,0 +1,12 @@
+
+
+const Pub = () => {
+
+  return (
+    <div className='Pub'>
+        <h1 className="display-5 text-light">Pub Page</h1>
+    </div>
+  );
+}
+
+export default Pub;
